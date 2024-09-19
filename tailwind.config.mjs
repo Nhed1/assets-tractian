d058ef28c7a2e8fccc17a07d186c4e2e957b0df7
leonardo <leonardogarreto@gmail.com>
2024-09-19 19:11:09 -0300

    chore: initial project setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@
 export default {
   content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        blue: {
+          500: '#2188FF',
+          700: '#023B78',
+          900: '#17192D'
+        },
+        red: '#ED3833',
+        green: '#52C41A'
+      }
+    }
   },
   plugins: []
 }
