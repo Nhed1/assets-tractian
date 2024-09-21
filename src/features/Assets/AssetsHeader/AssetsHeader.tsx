2437d843ffcab1b6060e2a51c20643a57001b113
leonardo <leonardogarreto@gmail.com>
2024-09-21 12:41:57 -0300

    feat(assets-header): add company name

--- src/features/Assets/AssetsHeader/AssetsHeader.tsx
+++ src/features/Assets/AssetsHeader/AssetsHeader.tsx
@@ -1,11 +1,16 @@
+import { useCompanyContext } from '../providers'
 import { AssetStatus } from './AssetStatus'
 
 export function AssetsHeader() {
+  const { companySelected } = useCompanyContext()
+
   return (
     <header className="flex h-12 items-center justify-between">
-      <div className="flex items-center">
+      <div className="flex items-center space-x-2">
         <p className="text-lg font-semibold">Ativos</p>
-        <p>/ company name</p>
+        {companySelected && (
+          <p className="text-gray-600">/ {companySelected?.name}</p>
+        )}
       </div>
 
       <div className="flex space-x-2">
