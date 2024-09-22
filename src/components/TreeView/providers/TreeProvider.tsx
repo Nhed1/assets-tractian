0e68121f3cc40617c017b59cf6bbbe2fed784ec6
leonardo <leonardogarreto@gmail.com>
2024-09-21 21:26:57 -0300

    fix(tree-provider): state not updating on tree

--- src/components/TreeView/providers/TreeProvider.tsx
+++ src/components/TreeView/providers/TreeProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useReducer } from 'react'
+import { ReactElement, useEffect, useReducer } from 'react'
 import { useTree } from '../hooks/useTree'
 import { TreeContext } from './useTreeContext'
 import { INode } from '../interfaces/node'
@@ -16,6 +16,10 @@ export const TreeProvider = ({
 }) => {
   const { treeReducer } = useTree()
   const [state, dispatch] = useReducer(treeReducer, initialData)
+
+  useEffect(() => {
+    dispatch({ type: 'INIT_DATA', data: initialData })
+  }, [initialData])
 
   return (
     <TreeContext.Provider value={{ state, dispatch, selectNode, node }}>
