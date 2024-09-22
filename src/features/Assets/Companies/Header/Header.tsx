e3fbb85126f5f257b6840592849f493e21267925
leonardo <leonardogarreto@gmail.com>
2024-09-22 00:21:36 -0300

    feat(companies-header): set first company as default

--- src/features/Assets/Companies/Header/Header.tsx
+++ src/features/Assets/Companies/Header/Header.tsx
@@ -2,14 +2,22 @@ import { TractianIcon } from 'assets/icons/TractianIcon'
 import { MenuButton } from './MenuButton'
 import { useGetCompanies } from '../hooks'
 import { Loading } from 'components/Loading'
+import { useEffect } from 'react'
+import { useCompanyContext } from 'features/Assets/providers'
 
 export function Header() {
+  const { setCompanySelected } = useCompanyContext()
+
   const {
     data: companies = [],
     isPending,
     isError,
     refetch
   } = useGetCompanies()
+
+  useEffect(() => {
+    setCompanySelected(companies[0])
+  }, [companies, setCompanySelected])
 
   return (
     <header className="flex h-12 items-center justify-between bg-blue-900 px-4 py-8 text-white">
